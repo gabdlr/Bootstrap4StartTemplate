910305492e705a2b8ce61c29d8e946c3f8f1fab2
gabdlr <gab.delosrios@gmail.com>
2021-08-14 21:57:21 -0300

    Implements PHP

---
+++ contacto.php
@@ -0,0 +1,40 @@
+<?php include "templates/header.php" ?>
+
+  <div class="container pt-4">
+      <div class="row no-gutters">
+          <div class="col-12 hero">
+              <img src="img/contacto.jpg" alt="contacto" class="img-fluid">
+              <h2 class="text-uppercase d-none d-md-block py-3 px-5">Contacto</h2>
+          </div>
+      </div>
+  </div>
+  <div class="container pt-4">
+      <div class="row justify-content-center">
+        <main class="col-lg-10 contenido-principal">
+            <h2 class="d-block d-md-none text-uppercase text-center">
+                Contacto
+            </h2>
+            <form action="" class="mt-5 p-5 formulario-contacto needs-validation" novalidate>
+              <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" id="nombre" placeholder="Tu nombre" required>
+                <div class="invalid-feedback">El nombre es obligatorio</div>
+              </div>
+              <div class="form-group">
+                <label for="email">E-mail:</label>
+                <input type="email" class="form-control" id="email" placeholder="Tu e-mail" required>
+                <div class="invalid-feedback">El email es obligatorio</div>
+                <div class="valid-feedback">Dirección de correo válidad</div>
+              </div>
+              <div class="form-group">
+                <label for="mensaje">Mensaje:</label>
+                <textarea name="mensaje" id="mensaje" cols="30" rows="2" class="form-control" required></textarea>
+                <div class="invalid-feedback">El mensaje no puede estar vacio</div>
+              </div>
+              <input type="submit" class="btn btn-primary text-uppercase" value="Enviar">
+            </form>
+      </main>
+    </div>
+  </div>
+
+  <?php include "templates/footer.php" ?>
---
+++ producto.php
@@ -0,0 +1,44 @@
+<?php include "templates/header.php" ?>
+
+    <div class="container pt-4">
+        <div class="row no-gutters">
+            <div class="col-12 hero">
+                <img src="img/producto_01.jpg" alt="nosotros" class="img-fluid">
+                <h2 class="text-uppercase d-none d-md-block py-3 px-5">Producto 1</h2>
+            </div>
+        </div>
+    </div>
+    <div class="container pt-4 ">
+        <div class="row no-gutters">
+            <main class="col-lg-8 contenido-principal pr-4">
+                <h2 class="d-block d-md-none text-uppercase text-center">
+                    Producto 1
+                </h2>
+                <p>Morbi maximus, metus vel tincidunt dictum, sem sapien feugiat urna, quis dignissim ligula orci quis
+                    enim. Suspendisse varius gravida magna consequat porta. Integer ut lectus nec ex dignissim
+                    efficitur. Duis justo nunc, mollis sit amet urna ac, porttitor cursus ex. Ut gravida vehicula
+                    finibus. Aenean in purus ut velit consectetur eleifend nec vitae risus. In semper lacus fermentum
+                    semper interdum. Aenean nibh dui, congue id nunc eget, lacinia luctus lectus. Donec feugiat, purus
+                    et fringilla scelerisque, eros quam vehicula mauris, vulputate pellentesque lorem quam eget ipsum.
+                    Donec eros magna, eleifend non mi et, congue feugiat arcu. Vivamus at felis dictum, cursus eros sit
+                    amet, egestas erat.</p>
+                <p>Phasellus vel nisi eu massa scelerisque pretium. Fusce bibendum purus sed justo pulvinar, et rutrum
+                    nulla pharetra. Suspendisse feugiat ornare volutpat. Phasellus rhoncus massa in ante sollicitudin
+                    ultrices. Ut feugiat ex id pellentesque varius. Sed venenatis rhoncus leo quis tincidunt. Quisque
+                    turpis augue, tempor quis leo ut, interdum mattis turpis.</p>
+            </main>
+            <aside class="col-lg-4 pt-4 pt-lg-0">
+                <div class="sidebar pt-5 descripcion-producto">
+                    <h2 class="text-center text-uppercase mt-4">
+                        Descripción
+                    </h2>
+                    <p class="text-center p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan feugiat commodo. Aenean
+                        diam libero, molestie nec varius eu, dapibus ut dui.</p>
+                        <h3 class="text-uppercase text-center mt-5 mb-0">Precio</h3>
+                        <p class="display-4 text-center p-2">$25.00</p>
+                </div>
+            </aside>
+        </div>
+    </div>
+
+    <?php include "templates/footer.php" ?>
